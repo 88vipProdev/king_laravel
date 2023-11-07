f159e165acfeee0b6b61bf32459d4062a8d994e6
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-07 21:59:59 +0700

    fix ProductController and productModel

--- app/Http/Controllers/admin/productController.php
+++ app/Http/Controllers/admin/productController.php
@@ -5,45 +5,44 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\productCreate;
-use App\Models\admin\createProduct;
+use App\Models\admin\productModel;
 
 class productController extends Controller
 {
-            protected  $product;
-            public function __construc(createProduct $product)
-            {
-                $this->createProduct = $product;
-            }
-            public function create()
-            {
-                return view("admin.createProduct");
-            }
+         protected $product ;
 
-            public function product(productCreate $request)
-            {
-                $path = null ; 
-                if($request->hasFile('image')){
-                    $image = $request->file('image');
-                    $path = $image->store('uploads','public');
-        
-                }
+         public function __construct(productModel $product)
+         {
+               $this->productModel = $product;
 
-                $listdata = [
-                    "name"=> $request->input('name'),
-                    'location' => isset($request->location) ? $request->location : null,
-                    'price'=>$request->input('price'),
-                    'image'=>$path ,
-                ];
-                // dd($listdata);
-                $product = createProduct::createTour($listdata);
-           
-            }
+         }
 
-            public function edit()
-            {
-                return view('admin.editProduct');
-            }
+         public function create()
+         {
+               return view("admin.createProduct");
+         }
+
+         public function product(productCreate $request)
+         {
+               $path = null;
+               if ($request->hasFile("image")) {
+                    $image = $request->file("image");
+                    $path = $image->store("uploads","public");
+               }
+
+               $listdata = [
+                "name"=> $request->input('name'),
+                'location' => isset($request->location) ? $request->location : null,
+                'price'=>$request->input('price'),
+                'image'=>$path ,
+            ];
+
+              $product = productModel :: createTour($listdata);
+         }
 
 
-
+         public function edit()
+         {
+            return view('admin.editProduct');
+         }
 }
---
+++ app/Models/admin/productModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+class productModel extends Model
+{
+    use HasFactory;
+
+            protected $fillable = ['name','image','location','price'];
+            protected $table = 'product';
+
+        public  static  function createTour($item)
+        {
+                $product = DB::table('product')->insert([
+                'name' => $item['name'],
+                'image' => isset($item['image']) ? $item['image'] : null,
+                'location' => $item['location'],
+                'price' => $item['price'],
+                ]);
+            return $product;
+        }
+
+
+
+}
