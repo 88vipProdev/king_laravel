c5dfe0cfb1de6434544df5501b29419fc22afba9
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-08 08:53:12 +0700

    update Checklogin MiddleWare

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkUser'=>\App\Http\Middleware\checkUser::class,
+        'checklogin'=>\App\Http\Middleware\checklogin::class,
 
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -31,33 +31,39 @@ use App\Http\Controllers\admin\ShowProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('viewRegister' ,[AuthController::class,'ViewRegister'])->name('viewRegister');
-Route::post('register',[AuthController::class ,'register'])->name('register');
-Route::get('viewlogin',[AuthController::class,'viewlogin'])->name('viewlogin');
-Route::post('login',[AuthController::class,'login'])->name('login');
-Route::get('home',[HomeController::class,'home'])->name('home');
-
-Route::prefix("user")->name("user.")->group(function () {
-    Route::get("showPage",[ShowPageController::class,"showPage"])->name("showPage");
-    Route::get("showTour" ,[ShowTourController::class, "showTour"])->name("showTour");   
-
-    
-});
+            Route::get('viewRegister' ,[AuthController::class,'ViewRegister'])->name('viewRegister');
+            Route::post('register',[AuthController::class ,'register'])->name('register');
+            Route::get('viewlogin',[AuthController::class,'viewlogin'])->name('viewlogin');
+            Route::post('login',[AuthController::class,'login'])->name('login');
+            Route::get('home',[HomeController::class,'home'])->name('home');
 
 
-Route::prefix('admin')->name('admin.')->group(function (){
-    Route::group(["middleware"=>"checkUser"], function (){
-        Route::get('index',[NewController ::class,'index'])->name('index');
-        Route::get('page',[pageController::class,'page'])->name('page');
-        Route::post('NewPage',[pageController::class,'NewPage'])->name('NewPages');
-        Route::get('create',[ProductController ::class,'create'])->name('create');
-        Route::post('product',[ProductController ::class,'product'])->name('product');
-        Route::get('show',[ShowProductController::class,'show'])->name('show');
-        Route::get('edit/{id}',[ProductController :: class ,'edit'])->name('edit');
-        route::post('update/{id}',[ProductController ::class,'update'])->name('update');
+            Route::prefix("user")->name("user.")->group(function () {
 
-    
-    });
-});
+                Route::group(["middleware"=>"checklogin"],function(){
+                    Route::get("showPage",[ShowPageController::class,"showPage"])->name("showPage");
+                    Route::get("showTour" ,[ShowTourController::class, "showTour"])->name("showTour");   
+                });
+                
+
+                
+            });
+
+
+            Route::prefix('admin')->name('admin.')->group(function (){
+                Route::group(["middleware"=>"checkUser"], function (){
+                    
+                        Route::get('index',[NewController ::class,'index'])->name('index');
+                        Route::get('page',[pageController::class,'page'])->name('page');
+                        Route::post('NewPage',[pageController::class,'NewPage'])->name('NewPages');
+                        Route::get('create',[ProductController ::class,'create'])->name('create');
+                        Route::post('product',[ProductController ::class,'product'])->name('product');
+                        Route::get('show',[ShowProductController::class,'show'])->name('show');
+                        Route::get('edit/{id}',[ProductController :: class ,'edit'])->name('edit');
+                        route::post('update/{id}',[ProductController ::class,'update'])->name('update');
+
+                
+                });
+            });
 
 
