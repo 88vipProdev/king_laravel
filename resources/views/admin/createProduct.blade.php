77b60c7f4184ddd108bf4464265c5cd5e0b392ed
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-10 21:06:00 +0700

    fix add product by category success

--- resources/views/admin/createProduct.blade.php
+++ resources/views/admin/createProduct.blade.php
@@ -4,58 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Thêm tour</title>
 </head>
 <body>
+    <h1>Thêm tour</h1>
 
-
-                <h1>Thêm tour</h1>
-
-        <div>
-            <form action="{{route('admin.product')}}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div>
-                    <label for="name">name</label><br>
-                    <input type="text" name="name" id=" placeholder="name">
-                    @error('name')
-                        <div>{{@message}}</div>
-                    @enderror
-                </div>
-                <div>
-                    <label for="image">Image</label><br>
-                    <input type="file" name="image" id="">
-                    @error('file')
-                        <div>{{@message}}</div>
-                    @enderror
-                </div>
-                <div>
-                    <label for="location">location</label><br>
-                    <input type="text" name="location" id="" placeholder="location">
-                    @error('location')
-                        <div>{{@message}}</div>
-                    @enderror
-                </div>
-                <div>
-                    <label for="price">price</label><br>
-                    <input type="text" name="price" id="" placeholder="price">
-                    @error('price')
-                        <div>{{@message}}</div>
-                    @enderror
-                </div>
-                <div>
-                    <label for="namecategory">namecategory</label><br>
-                    <select name="namecategory" id="namecategory" required>
-                        <option value="">category</option>
-                        @foreach ($categories as $category)
-                            <option value="{{$category->namecategory}}">{{ $category->namecategory }}</option>
-                        @endforeach
-                    </select>
-                    @error("namecategory")
-                        <div>{{@message}}</div>
-                    @enderror
-                </div>
-                <button type  = 'submit'>create</button>
-            </form type= "submit">
-        </div>
+    <div>
+        <form action="{{ route('admin.product') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <label for="name">Name</label><br>
+                <input type="text" name="name" id="name" placeholder="Name">
+                @error('name')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <label for="image">Image</label><br>
+                <input type="file" name="image" id="image">
+                @error('image')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <label for="location">Location</label><br>
+                <input type="text" name="location" id="location" placeholder="Location">
+                @error('location')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <label for="price">Price</label><br>
+                <input type="text" name="price" id="price" placeholder="Price">
+                @error('price')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div>
+                <label for="categories_id">Category</label><br>
+                <select name="categories_id" id="categories_id" required>
+                    <option value="">Select category</option>
+                    @foreach ($category as $id=>$name)
+                        <option value="{{$id}}">{{ $name }}</option>
+                    @endforeach
+                </select>
+                @error('categories_id')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <button type="submit">Create</button>
+        </form>
+    </div>
 </body>
 </html>
