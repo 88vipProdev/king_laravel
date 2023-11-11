d44a855f74f14e3c8f066ed715dd0094f6dc7ed7
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-11 15:09:44 +0700

    new fix update product

--- resources/views/admin/editProduct.blade.php
+++ resources/views/admin/editProduct.blade.php
@@ -10,26 +10,36 @@
         <h2>edit Product</h2>
 
         <div>
-                <form action="{{route("admin.update",['id'=>$product->id])}}" method="post" enctype="multipart/form-data">
-                    <div>
-                        <label for="name">name</label>
-                        <input type="text"   value = "name" id="" placeholder="name...">
-
-                    </div>
-                    <div>
-                        <label for="location">location</label>
-                        <input type="text" name="location" id = "" placeholder="location...">
-                    </div>
-                    <div>
-                        <label for="image">image</label>
-                        <input type="file" name="image" id="image">
-                    </div>
-                    <div>
-                        <label for="price">price</label>
-                        <input type="text" name="price" id="" placeholder="price">
-                    </div>
-                    <button type="submit">edit</button>
-                </form>
+            <form action="{{ route('admin.update', $product->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+           
+            
+                <label for="name">Tên sản phẩm</label><br>
+                <input type="text" name="name" value="{{ $product->name }}"><br>
+            
+                <label for="image">Ảnh</label><br>
+                <img src="{{ asset('uploads/.$listItem') }}" alt="image" sizes="full" srcset=""><br>
+                <input type="file" name="image" value="{{ $product->image }}"><br>
+            
+                <label for="location">Vị trí</label><br>
+                <input type="text" name="location" value="{{ $product->location }}"><br>
+            
+                <label for="price">Giá</label><br>
+                <input type="text" name="price" value="{{ $product->price }}"><br>
+            
+                <label for="categories_id">Category</label><br>
+                <select name="categories_id" id="categories_id" required><br>
+                    <option value="">Select category</option><br>
+                    @foreach ($category as $id=>$name)
+                        <option value="{{$id}}">{{ $name }}</option><br>
+                    @endforeach
+                </select>
+                @error('categories_id')
+                    <div>{{ $message }}</div>
+                @enderror
+            
+                <button type="submit">Cập nhật</button><br>
+            </form>
         </div>
 </body>
 </html>
