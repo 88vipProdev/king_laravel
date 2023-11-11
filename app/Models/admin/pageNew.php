9841e0aecabaf946079767f82437ed9bfd96cef3
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-11 23:18:24 +0700

    edit page model

--- app/Models/admin/pageNew.php
+++ app/Models/admin/pageNew.php
@@ -8,22 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 class pageNew extends Model
 {
-    use HasFactory;
+            use HasFactory;
 
 
-    protected $fillable = ['title','content', 'image'] ;
+            protected $fillable = ['title','content', 'image'] ;
 
-    protected $table='page_new';
+            protected $table='page_new';
+
+            
+            public static function showPage()
+            {
+                   $dataPage= DB::table('page_new')->get();
+                   return $dataPage;
+            }
+            
+            public static function create($data)
+            {
+                $page = DB::table('page_new')->insert([
+                    'title'=> $data['title'],
+                    'content'=> $data['content'],
+                    'image'=> $data['image'],
+                ]);
+                return $page; 
+            }
 
 
-     
-    public static function create($data)
-    {
-        $page = DB::table('page_new')->insert([
-            'title'=> $data['title'],
-            'content'=> $data['content'],
-            'image'=> $data['image'],
-        ]);
-        return $page; 
-    }
+            public static function edit($id, $data)
+            {
+                    $pagedata = pageNew::find($id);
+                    $pagedata->title = $data['title'];
+                    $pagedata->content = $data['content'];
+                    $pagedata->image = $data['image'];
+                    $pagedata->save();
+                    return $pagedata;
+            }
+
+
 }
