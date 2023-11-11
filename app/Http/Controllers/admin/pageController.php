e8abd23388339a3a3d8c582aa3f098f07ebfff64
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-11 23:20:17 +0700

    edit pageController

--- app/Http/Controllers/admin/pageController.php
+++ app/Http/Controllers/admin/pageController.php
@@ -11,39 +11,73 @@ use Illuminate\Http\Request;
 class pageController extends Controller
 {
 
-    public function __construct(pageNew $pageNew )
-    {
-        
-        $this->pageNew = $pageNew;
-    }
-    public function page()
-    {
-        return view("admin.page");
-    }
+                public function __construct(pageNew $pageNew )
+                {
+                    
+                    $this->pageNew = $pageNew;
+                }
 
-     public  function NewPage(pageRequest $request)  
-     {
-        
+               
+                public function page()
+                {
+                    return view("admin.page");
+                }
 
-        if($request->hasFile("image")){
-            $image = $request->file("image");
-            $path = $image->store("uploads","public");
+                public function showPage()
+                {
+                    $dataList = pageNew::showPage();
+                    return view("admin.showPage",compact("dataList"));
+                }
 
-        }else{
-            $path = null;
-        }
+                public  function NewPage(pageRequest $request)  
+                {
+                    
+
+                    if($request->hasFile("image")){
+                        $image = $request->file("image");
+                        $path = $image->store("uploads","public");
+
+                    }else{
+                        $path = null;
+                    }
 
 
-       
-          $datalist = [
+                
+                    $datalist = [
 
-            "title" =>$request->input("title"),
-            "content"=>$request->input("content"),
-            "image"=>$path,
-          ];
-  
+                        "title" =>$request->input("title"),
+                        "content"=>$request->input("content"),
+                        "image"=>$path,
+                    ];
+            
 
-          $pageNew = pageNew::create($datalist);
+                    $pageNew = pageNew::create($datalist);
 
-     }
+                }
+
+                public function EditNew($id)
+                {
+                    $dataPage = pageNew::find($id);
+                    return view("admin.editPage",compact("dataPage"));
+
+                }
+                public function EditPage(pageRequest $request ,$id)
+                {
+                        $pageId = pageNew::find($id);
+                        {
+                            if($request->hasFile("image")){
+                                $image = $request->file("image");
+                                $path = $image->store("uploads","public");
+                            }
+    
+                            $datapage =[
+    
+                                "title"=>$request->input("title"),
+                                "content"=>$request->input("content"),
+                                "image"=>$path,
+                            ];
+                        }
+
+                        pageNew::edit($datapage,$id);
+                }
 }
