3004292c9f54779d44c60d31b98d3e610d4deda6
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-13 18:37:14 +0700

    update detail Model

--- app/Models/admin/detailModel.php
+++ app/Models/admin/detailModel.php
@@ -41,5 +41,18 @@ class detailModel extends Model
                 $detaildata = detailModel::all();
                 return $detaildata ;
          }
+
+         public static function updateDetail($data,$id)
+         {
+                $detail = detailModel::find($id);
+                $detail->namedetail = $data["namedetail"];
+                $detail->review = $data["review"];
+                $detail->image = $data["image"];
+                $detail->location = $data["location"];
+                $detail->price = $data["price"];
+                $detail->productId = $data["productId"];
+                $detail->save() ;
+                return $detail ;
+         }
    
 }
