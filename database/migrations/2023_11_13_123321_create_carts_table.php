20ed45cbcb94e0667d47a2070c30d0499e572d02
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-15 15:23:40 +0700

    add columm table cart

--- database/migrations/2023_11_13_123321_create_carts_table.php
+++ database/migrations/2023_11_13_123321_create_carts_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->integer('quantity');
+            $table->string ('name');
+            $table->string ('image');
+            $table->string ('price');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
