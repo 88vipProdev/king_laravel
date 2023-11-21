903f6fb3fa4ac145b568b81d18f184a51e541671
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-21 19:43:34 +0700

    fix commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/header';
-import Main from './Components/main';
 
-function App() {
-  return (
-       <div>
-          
-        <Header
-        home="/home"
-        contact="/contact"
-        about="/about"
-        news="/news"
-        login="/login"
-        register="/register"
-        logo="/logo.png"
-        cart = "/cart"
-      /> 
-        <div>
-            <main/>
-        </div>
-       </div>
-      
-      
-  );
-}
-
-export default App;
-=======
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Products}  from './components /product';
 import ProductTSX from './components /product';
+
+
 
 
 function App() {
@@ -55,4 +26,4 @@ function App() {
 }
 
 export default App;
->>>>>>> 6ac1ecf (call api)
+
