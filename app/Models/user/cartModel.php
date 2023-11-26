378972d0540b218f1df8343a9a5b5a7c14a5d901
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-26 19:37:09 +0700

    remove cart controller

--- app/Models/user/cartModel.php
+++ app/Models/user/cartModel.php
@@ -13,7 +13,7 @@ class cartModel extends Model
 {
         use HasFactory;
         protected $table = "carts";
-        protected $fillable = ["id","user_id"] ;
+        protected $fillable = ["id","user_id","product_id"] ;
 
 
         public function product()
@@ -30,4 +30,45 @@ class cartModel extends Model
         
               return $cartItems  ; 
         }
+
+        public static function deleteCart($id)
+        {
+               
+            try {
+
+                // Tìm cart item 
+                $cartItem = CartModel::findOrFail($id);
+              
+                // Lấy số lượng hiện tại
+                $currentQuantity = $cartItem->quantity;
+              
+                // Kiểm tra số lượng > 0 mới giảm được
+                if($currentQuantity > 0) {
+              
+                  // Giảm số lượng
+                  $quantity = $currentQuantity - 1;  
+              
+                  // Cập nhật lại số lượng mới
+                  CartModel::where('id', $id)
+                           ->update(['quantity' => $quantity]);
+              
+                  // Trả về thành công
+                  return response()->json(['message' => 'Quantity updated']);
+              
+                } else {
+              
+                  // Trả về nếu số lượng đã = 0
+                  return response()->json(['message' => 'Quantity reached zero'], 400);
+              
+                }
+              
+              } catch (\Exception $e) {
+              
+                return error_response();
+              
+              }
+
+        }
+
+     
 }
