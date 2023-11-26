1c785404dbd19767348449cfdb3829ecb96a4525
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-26 17:30:21 +0700

    fix add to cart and update

--- app/Http/Controllers/user/cartController.php
+++ app/Http/Controllers/user/cartController.php
@@ -29,24 +29,42 @@ class cartController extends Controller
             }
 
             
-            public function render(cartRequest $request , string $id)
-            {
+            public function render(Request $request, $productId) {
 
-                $id_user = Auth::user()->id;
-                if ($request->hasFile("image")) {
-                    $image = $request->file("image");
-                    $path = $image->store("uploads","public");
+                // Get authenticated user
+                $userId = auth()->id();
+              
+                // Find the product 
+                $product = product::find($productId);
+              
+                // Check if this product is already in cart
+                $cart = cartModel::where('user_id', $userId)
+                           ->where('product_id', $product->id)  
+                           ->first();
+              
+                if($cart) {
+              
+                  // Product already exists in cart, update quantity  
+                  $cart->quantity += 1;
+                  $cart->save();
+              
+                } else {
+              
+                  // Product not in cart yet, add new cart item
+              
+                  $cart = new cartModel();
+                  $cart->user_id = $userId;
+                  $cart->product_id = $product->id;
+                  $cart->image = $product->image;  
+                  $cart->quantity = 1;
+                  $cart->save();
+              
                 }
-                $cart = new cartModel() ;
-                $product = product::find($id);
-                $cart->image = $image;
-                $cart->user_id = $id_user;
-                $cart->product_id = $id;
-                $cart->quantity = 1;
-                $cart->save();
+              
+                // Redirect back to previous page
                 return redirect()->back();
-            }
-
+              
+              }
             public function showCart()
             {
                     $show = cartModel::showCart();
@@ -55,26 +73,7 @@ class cartController extends Controller
                 
             }
 
-            public function updateCart(Request $requset ,string $id)
-            {   
-                $id_user = Auth::user()->id;
-                $cart = new cartModel();
-                if($cart)
-                {
-                    $cart->quantity +=1 ;
-                    $cart->save(); 
-                }
-                else
-                {
-                    $product = product::find($id);
-                    $cart->user_id = $id_user;
-                    $cart->product_id = $id;
-                    $cart->quantity = 1;
-                    $cart->save();
-                    return redirect()->back();
-                }
-
-            }
+     
 
 
 }
