1be799de1048ef5d7e65f112370c5a7aa7f06d96
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-11-30 16:17:57 +0700

    fix check user middleware

--- app/Http/Middleware/checkUser.php
+++ app/Http/Middleware/checkUser.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Models\User;
+use Auth;
 
 class checkUser
 {
@@ -15,12 +16,25 @@ class checkUser
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
-    {   
-        // dd($request->user()->isAdmin());
-        if(!$request ->user()->isAdmin()){
+    {  
         
-            abort(403,"Unauthorized");
-        }
-        return $next($request);
+   
+        // Kiểm tra xem người dùng đã xác thực hay chưa
+            if (!Auth::check()) {
+                // Người dùng chưa đăng nhập, chuyển hướng về trang đăng nhập
+                return redirect()->route('viewlogin');
+            }
+
+            // Kiểm tra xem người dùng có quyền admin hay không
+            if (Auth::user()->isAdmin()) {
+                // Người dùng là admin, chuyển hướng tới trang admin
+                return redirect()->route('admin.index');
+            }
+
+            // Người dùng không phải là admin, chuyển hướng hoặc trả về lỗi 403 Unauthorized
+            return abort(403, 'Unauthorized');
     }
-}
+    }
+
+    
+
