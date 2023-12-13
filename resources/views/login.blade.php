a61dbab4b3a56b50dfa59ac66c8e9195033de886
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-12-13 21:53:53 +0700

    fix view login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,35 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-                <h1>trang login</h1>
 
-        <div>
-            <form action ="{{route("login")}}" method="POST">
-                @csrf
-                <div>
-                    <lable for= "email">email</lable><br>
-                    <input type="text" name="email" id = "" placeholder="username"><br>
-                    @error("username")
-                        <div>{{$message}}</div>
-                    @enderror
-                </div>
-                <div>
-                    <lable for="password">password</lable><br>
-                    <input type="password" name="password" id = "" placeholder="password"><br>
-                    @error("password")
-                        <div>{{$message}}</div>
-                    @enderror
-                </div>
-
-                <input type = "submit" value="login">
-                
-            </form value="submit">
+<div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</div>
+<form action ="{{route("login")}}" method="POST">
+@csrf
+<section class="vh-100" style="background-color: #508bfc;">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+  
+              <h3 class="mb-5">Sign in</h3>
+  
+              <div class="form-outline mb-4">
+                <input type="text"  name = "email" id="typeEmailX-2" class="form-control form-control-lg" />
+                <label class="form-label" for="typeEmailX-2">Email</label>
+                @error("email")
+                     <div>{{$message}}</div>
+                @enderror
+              </div>
+  
+              <div class="form-outline mb-4">
+                <input type="text" name="password" id="typePasswordX-2" class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX-2">Password</label>
+                @error("password")
+                <div>{{$message}}</div>
+                @enderror
+              </div>
+  
+              <!-- Checkbox -->
+              <div class="form-check d-flex justify-content-start mb-4">
+                <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+                <label class="form-check-label" for="form1Example3"> Remember password </label>
+              </div>
+  
+              <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+  
+              <hr class="my-4">
+  
+              <button class="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;"
+                type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
+              <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;"
+                type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
+  
+            </div>
+          </div>
         </div>
-</body>
-</html>
+      </div>
+    </div>
+  </section>
+</form value="submit">
+</div>
+</div>
+
+
+       
+
